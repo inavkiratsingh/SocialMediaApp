fbee02d2479e37acfa177af362c2bc2b280cf864
Ubuntu <navs6164@gmail.com>
2024-08-06 22:09:21 +0530

    models

---
+++ backend/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, { mongo } from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Post'
+    }
+});
+
+export default comment = mongoose.model('Comment', commentSchema);
