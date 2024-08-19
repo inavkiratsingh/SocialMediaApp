c5440e879280cb9a43bf62d5ca18981d0c863bd8
Ubuntu <navs6164@gmail.com>
2024-08-20 00:09:56 +0530

    backend frontend

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,20 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path"
 
 dotenv.config({});
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    return res.status(200).json({
-        message: "I m comming form backend",
-        success: true
-    })
-})
+const __dirname = path.resolve();
+
+// app.get('/', (req,res) => {
+//     return res.status(200).json({
+//         message: "I m comming form backend",
+//         success: true
+//     })
+// })
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,6 +35,12 @@ app.use(cors(corsOptions))
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 
 server.listen(PORT,() => {
